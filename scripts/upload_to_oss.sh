ab3b8ad6653f7c66e9eacc997d6b7e8796cfb120
zhouqinren.zqr <zhouqinren.zqr@alibaba-inc.com>
2026-02-05 12:42:49 +0800

    minor: clean obsolete files

--- scripts/upload_to_oss.sh
+++
@@ -1,7 +0,0 @@
-# Upload the contents of the local './out/' directory to the OSS bucket named 'zvec-website'.
-#
-# Prerequisites:
-# - ossutil must be installed and properly configured with valid credentials.
-# - The OSS endpoint should be set to: https://oss-cn-hongkong.aliyuncs.com
-
-ossutil sync ./out/ oss://zvec-website -f
